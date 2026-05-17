3340fbe54a0df23a642b2e06dfb98132d14e42b4
indu024 <irindiranipv@gmail.com>
2026-05-17 13:14:39 +0530

    Made changes to app

--- app/app.js
+++ app/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('DevOps Project Running');
+app.use(express.static('public'));
+
+app.get('/api/message', (req, res) => {
+  res.json({
+    message: 'DevOps Full Stack App Running in Kubernetes'
+  });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(3000, () => {
-  console.log('Server started');
+  console.log('Server started on port 3000');
 });
